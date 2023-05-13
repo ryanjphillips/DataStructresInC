18fc8388a45c055a17ecd7f6014805eac9ed9790
Ryan Phillips <ryan.phillips.j@gmail.com>
2023-05-13 16:24:57 -0500

    UPDATE: Update Linked List with Deletion Code.

--- LinkedList/LinkedList.h
+++ LinkedList/LinkedList.h
@@ -10,3 +10,4 @@ int main();
 struct node *createList();
 struct node *addToBeginningOfList(struct node *list, int n);
 struct node *searchList(struct node *list, int n); 
+struct node *deleteFromList(struct node *list, int n); 
--- LinkedList/linkedList.c
+++ LinkedList/linkedList.c
@@ -40,3 +40,24 @@ struct node *searchList(struct node *list, int n) {
    }
    return(list);
 }
+
+struct node *deleteFromList(struct node *list, int n) {
+   struct node *cur;
+   struct node *prev;
+
+   for (cur = list, prev = NULL; cur != NULL && cur->value != n; prev = cur, cur = cur->next) {
+
+      if (cur == NULL) {
+         return list;
+      }
+
+      if (prev == NULL) {
+         list = list->next;
+      } else {
+         prev->next = cur->next;
+      }
+   }
+
+   free(cur);
+   return list;
+}
